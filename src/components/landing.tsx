22053110da89d673935a1241bcbd302c16388316
Addy2048 <aazaliwa@gmail.com>
2024-10-15 17:42:12 +0300

    fix: make responsive

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -14,10 +14,10 @@ export default function Landing() {
         alt=""
       />
       <div className="h-full w-full bg-black/70 absolute top-0 right-0 backdrop-blur-sm flex flex-col justify-center items-center">
-        <p className="text-8xl font-bold text-center">
+        <p className="text-2xl md:text-6xl xl:text-8xl font-bold text-center px-4">
           Meet Adiel Elifelt Azaliwa
         </p>
-        <div className="flex flex-row justify-center gap-8 mt-4">
+        <div className="hidden md:flex flex-row justify-center gap-8 mt-4">
           <Link href={"https://github.com/Addy2048"}>
             <FaGithub color="#FFFFFF" size={48} />
           </Link>
@@ -35,16 +35,34 @@ export default function Landing() {
             <FaInstagram color="#FFFFFF" size={48} />
           </Link>
         </div>
+        <div className="flex flex-row justify-center gap-4 md:gap-8 mt-4 md:hidden">
+          <Link href={"https://github.com/Addy2048"}>
+            <FaGithub color="#FFFFFF" size={24} />
+          </Link>
+          <Link href={"https://x.com/AdielElifelet"}>
+            <FaXTwitter color="#FFFFFF" size={24} />
+          </Link>
+          <Link href={"https://www.linkedin.com/in/adiel-azaliwa-2093921b9/"}>
+            <FaLinkedin color="#FFFFFF" size={24} />
+          </Link>
+          <Link
+            href={
+              "https://www.instagram.com/adiel_elifelet/profilecard/?igsh=MXZlMGhicGcyZWoxOA=="
+            }
+          >
+            <FaInstagram color="#FFFFFF" size={24} />
+          </Link>
+        </div>
         <div className="mt-8 flex flex-row gap-12">
           <a
             href={"/AdielCV.pdf"}
             target="_blank"
             rel="noopener noreferrer"
-            className="border-2 border-white bg-transparent rounded-[40px] px-4 py-2 text-xl font-bold"
+            className="border-2 border-white bg-transparent rounded-[40px] px-4 py-2 text-lg md:text-xl font-bold"
           >
             Get Resume
           </a>
-          <button className="border-2 border-white bg-transparent rounded-[40px] px-4 py-2 text-xl font-bold">
+          <button className="border-2 border-white bg-transparent rounded-[40px] px-4 py-2 text-lg md:text-xl font-bold">
             Contact
           </button>
         </div>
