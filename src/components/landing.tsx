d75346e01703b7f1dcde359161abc879a93b4da8
Addy2048 <aazaliwa@gmail.com>
2024-10-15 17:32:16 +0300

    feat: landing

---
+++ src/components/landing.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import LandingImage from "../../public/coding.jpg";
+import Link from "next/link";
+import { FaGithub, FaInstagram, FaLinkedin, FaXTwitter } from "react-icons/fa6";
+
+export default function Landing() {
+  return (
+    <div className="w-screen h-screen relative">
+      <Image
+        src={LandingImage}
+        height={5000}
+        width={5000}
+        className="w-full h-full object-cover"
+        alt=""
+      />
+      <div className="h-full w-full bg-black/70 absolute top-0 right-0 backdrop-blur-sm flex flex-col justify-center items-center">
+        <p className="text-8xl font-bold text-center">
+          Meet Adiel Elifelt Azaliwa
+        </p>
+        <div className="flex flex-row justify-center gap-8 mt-4">
+          <Link href={"https://github.com/Addy2048"}>
+            <FaGithub color="#FFFFFF" size={48} />
+          </Link>
+          <Link href={"https://x.com/AdielElifelet"}>
+            <FaXTwitter color="#FFFFFF" size={48} />
+          </Link>
+          <Link href={"https://www.linkedin.com/in/adiel-azaliwa-2093921b9/"}>
+            <FaLinkedin color="#FFFFFF" size={48} />
+          </Link>
+          <Link
+            href={
+              "https://www.instagram.com/adiel_elifelet/profilecard/?igsh=MXZlMGhicGcyZWoxOA=="
+            }
+          >
+            <FaInstagram color="#FFFFFF" size={48} />
+          </Link>
+        </div>
+        <div className="mt-8 flex flex-row gap-12">
+          <a
+            href={"/AdielCV.pdf"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="border-2 border-white bg-transparent rounded-[40px] px-4 py-2 text-xl font-bold"
+          >
+            Get Resume
+          </a>
+          <button className="border-2 border-white bg-transparent rounded-[40px] px-4 py-2 text-xl font-bold">
+            Contact
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
@@ -8,12 +9,74 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-green":
+          "linear-gradient(90deg, rgba(4, 75, 17, 1), rgba(8, 153, 46, 1))",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#2AAA22",
+        gray: "#D9D9D9",
+        dark: "#222222",
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        xxs: "320px",
+        // => @media (min-width: 320px) { ... }
+
+        xs: "420px",
+        // => @media (min-width: 420px) { ... }
+
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        "2md": "927px",
+        // => @media (min-width: 927px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1280px) { ... }
+      },
+      borderWidth: {
+        DEFAULT: "1px",
+        0: "0",
+        2: "2px",
+        3: "3px",
+        4: "4px",
+        6: "6px",
+        8: "8px",
+        10: "10px",
+        12: "12px",
+        14: "14px",
+        16: "16px",
+        18: "22px",
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: false,
+    darkTheme: "dark",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
+  },
 };
 export default config;
